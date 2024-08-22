675b9055fa13e1f4c3c56030c11932b6ce40eda0
Soojin6943 <sjhan0711@gmail.com>
2024-08-22 13:26:18 +0900

    [add] 상세 페이지 추

--- sbb-1/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb-1/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +24,14 @@ public class QuestionController {
 		// 모델 객체는 자바 클래스와 템플릿 간의 연결고리 역할 (모델 객체에 값을 담아 두면 템플릿에서 그 값을 사용할 수 있음)	
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+	
+	@GetMapping(value = "/question/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		// service에서 가져오고 controller에서 선언?하고 모델로 해서 템플릿 간 연결 후 사용?
+		
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 }
--- sbb-1/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb-1/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -6,14 +6,27 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+import com.mysite.sbb.DataNotFoundException;
+
 @RequiredArgsConstructor
 @Service
+// 생성 클래스를 서비스로 만들기 위해서는 애너테이션을 붙이면 
 public class QuestionService {
 	
 	private final QuestionRepository questionRepository;
 	
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
+	}
+	
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
 	}
 
 }
