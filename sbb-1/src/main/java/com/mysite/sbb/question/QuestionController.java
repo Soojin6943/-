7f6ec0cfe0643e088b035ec7b4cc2b53ae6ed904
Soojin6943 <sjhan0711@gmail.com>
2024-08-25 23:30:27 +0900

    [add] form 유효성 처리

--- sbb-1/src/main/java/com/mysite/sbb/question/QuestionController.java
+++ sbb-1/src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
+
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
@@ -40,14 +43,16 @@ public class QuestionController {
 	}
 	
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 	
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam(value="subject") String subject, @RequestParam(value="content") String content) {
-		
-		this.questionService.create(subject, content);
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; //질문 저장 후 질문 목록으로 이동 
 	}
 }
